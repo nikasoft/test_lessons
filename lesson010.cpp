d89e3cb48b31e00cdcc47f88a165086c24ccf2a5
nikasoft <48128830+nikasoft@users.noreply.github.com>
2019-03-06 21:10:28 +0200

    Add files via upload

--- lesson010.cpp
+++ lesson010.cpp
@@ -25,32 +25,57 @@ class Candy{
 
     void info()
     {
-        cout << "====================================\n";
-        cout << "Конфета: " << name << "(" << kkal << "kkal/gr)\n";
-        cout << "Цена:    " << price <<"MDL\n";
-        cout << "====================================\n";
+        char unsigned i; // диапазон от 0 до 255.
+        char ch1(201), ch2(205), ch3(187), ch4(186), ch5(200), ch6(188);
+        // cout << "====================================\n";
+        // cout << "| Candy: " << name << "(" << kkal << "kkal/gr)  |\n"; 
+        // cout << "| Price:    " << price <<"MDL                |\n";
+        // cout << "====================================\n";
+
+        cout << ch1; for (i=0; i<34; i++) cout << ch2; cout << ch3 << endl;
+        cout << ch4 << " Candy: " << name << "(" << kkal << "kkal/gr)  " << ch4 << endl; 
+        cout << ch4 << " Price:       " << price << "MDL             " << ch4 << endl; 
+        cout << ch4 << " Country:     " << country << "                  " << ch4 << endl; 
+        cout << ch5; for (i=0; i<34; i++) cout << ch2; cout << ch6 << endl;
+
     }
     //////////OPERATOR OVERLOADING//////////////////
 
-    bool operator > (Candy other) 
+    bool operator > (Candy other) // Переопределяем оператор <
     {
         return price < other.price && kkal < other.kkal;
     }
     // ДЗ - 1 Переопределить < и ==
 
-    
+    bool operator == (Candy other) // Переопределяем оператор ==
+    {
+        return price == other.price && kkal == other.kkal;
+    }
+
+    bool operator < (Candy other) // Переопределяем оператор >
+    {
+        return price > other.price && kkal > other.kkal;
+    }
+
 };
 
 int main()
 {
-    setlocale(LC_ALL,"RUS");
-    // cout << "Крассная конфета ";
     Candy red_candy ("Red Chups", 370.05, 27.50, "UA");
     Candy black_candy ("Black Cups Bomb", 600, 100, "IL");
     red_candy.info();
-    if (red_candy > black_candy)
+        if (red_candy > black_candy)
     {
         cout << "Red candy is better!";
     }
     
+        if (red_candy == black_candy)
+    {
+        cout << "Red candy is same!";
+    }
+        if (red_candy < black_candy)
+    {
+        cout << "Red candy is worse!";
+    }
+
 }
