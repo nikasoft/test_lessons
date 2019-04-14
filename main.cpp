0128880ec2691bb93a858dc938247c7edeac24ec
nikasoft <48128830+nikasoft@users.noreply.github.com>
2019-04-14 18:01:33 +0300

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,4 +1,43 @@
 #include <iostream>
+#include <stdlib.h>
+
+/* for windows*/
+#ifdef _WIN64
+#include <conio.h>
+/* for windows*/
+#elif __linux
+/* for Linux*/
+#include <termios.h>
+#include <unistd.h>
+#include <stdio.h>
+/* reads from keypress, doesn't echo */
+int getch(void)
+{
+    struct termios oldattr, newattr;
+    int ch;
+    tcgetattr( STDIN_FILENO, &oldattr );
+    newattr = oldattr;
+    newattr.c_lflag &= ~( ICANON | ECHO );
+    tcsetattr( STDIN_FILENO, TCSANOW, &newattr );
+    ch = getchar();
+    tcsetattr( STDIN_FILENO, TCSANOW, &oldattr );
+    return ch;
+}
+/* reads from keypress, echoes */
+int getche(void)
+{
+    struct termios oldattr, newattr;
+    int ch;
+    tcgetattr( STDIN_FILENO, &oldattr );
+    newattr = oldattr;
+    newattr.c_lflag &= ~( ICANON );
+    tcsetattr( STDIN_FILENO, TCSANOW, &newattr );
+    ch = getchar();
+    tcsetattr( STDIN_FILENO, TCSANOW, &oldattr );
+    return ch;
+}
+/* for Linux*/
+#endif
 
 using namespace std;
     
@@ -8,7 +47,9 @@ class Thing
     int r;
     int c;
     string what;
-    Thing(){}
+    Thing(){
+        what = "thing";
+    }
     Thing(int pr, int pc)
     {    
         if (pr>=0 && pr<=9) r = pr;
@@ -22,36 +63,6 @@ class Thing
     }
 
 };
-
-class Pacman: public Thing
-{
-    public:
-    Pacman(int pr, int pc): Thing(pr,pc){
-    what = "pacman";
-    }
-   
-
-};
-
-class Mine: public Thing
-{
-    public:
-    Mine(int pr, int pc): Thing(pr,pc){
-    what = "mine";
-    }
-    
-};
-
-class MedKit: public Thing
-{
-    public:
-    MedKit(int pr, int pc): Thing(pr,pc){
-        what = "madkit";
-    }
-
-
-};
-//karta igri
 
 class GameMap
 {
@@ -95,7 +106,13 @@ class GameMap
 
 
     void show()
-    {
+    {   
+        #ifdef _WIN64
+            system("cls");
+        #elif __linux    
+            system("clear"); //linux
+        #endif
+        
         cout << divider();
         for (int r = 0; r <= 9; r++)
         {
@@ -107,8 +124,18 @@ class GameMap
                 }
                 else if (grid[r][c].what == "pacman")
                 {
-                    cout << " + ";
+                    cout << " C ";
                 }
+                else if (grid[r][c].what == "mine")
+                {
+                    cout << "\033[1;31m"; //mine color is red
+                    cout << " x ";
+                    cout << "\033[1;32m"; //set default color
+                }                
+                else if (grid[r][c].what == "medkit")
+                {
+                    cout << " + ";
+                }                
                 // else cout << " + ";
             }
             cout << endl;
@@ -119,14 +146,91 @@ class GameMap
     }
 };
 
+GameMap gm;
+class Pacman: public Thing
+{
+    public:
+    Pacman(int pr, int pc): Thing(pr,pc){
+    what = "pacman";
+    }
+   void moveDown()
+   {
+       gm.grid[++r][c] = gm.grid[r][c];
+       gm.grid[r-1][c] = Thing(); // clear the place
+      
+   }
+   void moveUp()
+   {
+       gm.grid[--r][c] = gm.grid[r][c];
+       gm.grid[r+1][c] = Thing(); // clear the place
+      
+   }
+   void moveLeft()
+   {
+       gm.grid[r][--c] = gm.grid[r][c];
+       gm.grid[r][c+1] = Thing(); // clear the place
+      
+   }
+   void moveRight()
+   {
+       gm.grid[r][++c] = gm.grid[r][c];
+       gm.grid[r][c-1] = Thing(); // clear the place
+      
+   }
+
+};
+
+class Mine: public Thing
+{
+    public:
+    Mine(int pr, int pc): Thing(pr,pc){
+    what = "mine";
+    }
+    
+};
+
+class MedKit: public Thing
+{
+    public:
+    MedKit(int pr, int pc): Thing(pr,pc){
+        what = "medkit";
+    }
+
+
+};
+//karta igri
 
 
 int main ()
 {
-    Pacman pac(2,2);
-    GameMap gm;
-
+    Pacman pac(5,5);
     gm.add(pac);
+    gm.add(Mine(6,5));
+    gm.add(MedKit(7,5));
     gm.show();
    
+
+
+
+   while(1){
+       gm.show();
+       cout << "Left \"A\"";
+       char c = getch();
+       switch (c)
+       {
+           case 'w' : pac.moveUp();
+               break;
+           case 'a' : pac.moveLeft();
+               break;
+           case 'd' : pac.moveRight();
+               break;
+           case 's' : pac.moveDown();
+               break;
+       
+           
+       }
+   }
+   
+    gm.show();
+    
 }
